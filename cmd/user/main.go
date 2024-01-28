b87561e9bee6c2056fbe8046ccf6099c84ba1bac
zty <2745969694@qq.com>
2024-01-28 19:28:56 +0800

    ‘备注信息’

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	"github.com/kitex-contrib/registry-nacos/v2/registry"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 	"log"
 	userdemo "micro/kitex_gen/userdemo/userservice"
 )
 
 func main() {
-	svr := userdemo.NewServer(new(UserServiceImpl))
 
-	err := svr.Run()
-
-	if err != nil {
-		log.Println(err.Error())
+	sc := []constant.ServerConfig{
+		*constant.NewServerConfig("127.0.0.1", 8848),
 	}
+
+	cc := constant.ClientConfig{
+		NamespaceId:         "public",
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogLevel:            "info",
+	}
+
+	cli, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &cc,
+			ServerConfigs: sc,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	svr := userdemo.NewServer(new(UserServiceImpl),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "user"}),
+		server.WithRegistry(registry.NewNacosRegistry(cli)),
+	)
+	if err := svr.Run(); err != nil {
+		log.Println("server stopped with error:", err)
+	} else {
+		log.Println("server stopped")
+	}
+
 }
